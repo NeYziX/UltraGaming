37066741f455df7b65d844ce4e84aee24e5d8fcc
NeYziX <38168101+NeYziX@users.noreply.github.com>
2018-05-09 17:20:29 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -73,7 +73,7 @@ bot.on("message", function(message) {
             let xo03 = xoargs.join(" ")
             var xo02 = message.guild.channels.find('name', 'u-chat');
             if(!xo02) return message.reply("Le channel **u-chat** est introuvable")
-            if(message.channel.name !== 'u-chat') return message.reply("Commande à effectuer dans **u-chat**)
+            if(message.channel.name !== 'u-chat') return message.reply("Commande à effectuer dans **u-chat**")
             if(!xo03) return message.reply("Merci d'écrire un message qui sera envoyé à tous les serveurs où je suis.")
             var embed = new Discord.RichEmbed()
             .setColor("0x88CC14")
