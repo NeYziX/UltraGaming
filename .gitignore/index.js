0cf53e106e57b0306a3f73e7a3e3348a946c7fb7
NeYziX <38168101+NeYziX@users.noreply.github.com>
2018-05-10 18:06:54 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -93,6 +93,24 @@ bot.on('message', message => {
     if (message.content === prefix + "ping"){
         message.channel.sendMessage("Temps de latence avec le serveur : " + `${message.createdTimestamp - Date.now()}` + "MS");  
     }
+  
+    if (message.content.startsWith(prefix + "sondage")) {
+        let args = message.content.split(" ").slice(1);
+        let thingToEcho = args.join(" ")
+        var embed = new Discord.RichEmbed()
+            .setTitle("Nouveau Sondage")
+            .addField(thingToEcho, "Répondre avec :white_check_mark: ou :x:")
+            .setColor("0x00FF00")
+            .setTimestamp()
+        message.guild.channels.find("name", "sondage").sendEmbed(embed)
+        .then(function (message) {
+            message.react("✅")
+            message.react("❌")
+        }).catch(function() {
+        });
+        }else{
+            return message.reply("Tu n'as pas la permissions.")
+        }}
 });      
 
 bot.on('message', message => {
