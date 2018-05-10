38673ea6015f06470871f9c7eea96a8efa3f1ce5
NeYziX <38168101+NeYziX@users.noreply.github.com>
2018-05-10 18:13:21 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -93,21 +93,6 @@ bot.on('message', message => {
     if (message.content === prefix + "ping"){
         message.channel.sendMessage("Temps de latence avec le serveur : " + `${message.createdTimestamp - Date.now()}` + "MS");  
     }
-  
-    if (message.content.startsWith(prefix + "sondage")) {
-        let args = message.content.split(" ").slice(1);
-        let thingToEcho = args.join(" ")
-        var embed = new Discord.RichEmbed()
-            .setTitle("Nouveau Sondage")
-            .addField(thingToEcho, "Répondre avec :white_check_mark: ou :x:")
-            .setColor("0x00FF00")
-            .setTimestamp()
-        message.guild.channels.find("name", "sondage").sendEmbed(embed)
-        .then(function (message) {
-            message.react("✅")
-            message.react("❌")
-        }).catch(function() {
-    }
 });
 
 bot.on('message', message => {
